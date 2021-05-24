c6f70a83c2c8b9d98f7a86c076a3e53c5409561c
shivam damre <shivamdamre913@gmail.com>
2021-05-24 21:23:46 +0530

    change in js file

--- TodoList/todo.js
+++ TodoList/todo.js
@@ -1,22 +1,22 @@
-$("ul").on("click", "li",function(){
-	$(this).toggleClass("selected")
-})
+// $("ul").on("click", "li",function(){
+// 	$(this).toggleClass("selected")
+// })
 
-$("ul").on("click", "span", function(event){
-	$(this).parent().fadeOut(500, function(){
-		$(this).remove()
-	});
-	event.stopPropagation()
-});
+// $("ul").on("click", "span", function(event){
+// 	$(this).parent().fadeOut(500, function(){
+// 		$(this).remove()
+// 	});
+// 	event.stopPropagation()
+// });
 
-$("input[type='text'").on("keypress", function(event){
-	if(event.which == 13){
-		var todotext = $(this).val()
-		$(this).val('')
-		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todotext + "</li>")
-	}
-})
+// $("input[type='text'").on("keypress", function(event){
+// 	if(event.which == 13){
+// 		var todotext = $(this).val()
+// 		$(this).val('')
+// 		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todotext + "</li>")
+// 	}
+// })
 
-$(".fa-plus").on("click", function(){
-	$("input[type='text'").fadeToggle()
-});
+// $(".fa-plus").on("click", function(){
+// 	$("input[type='text'").fadeToggle()
+// });
